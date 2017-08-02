d02556944c33c4df13e00cacd3dd5d3c98b325c9
Tristan Constable <tristan.m.constable@gmail.com>
2017-08-02 12:01:21 +0200

    Created Teacher Class

---
+++ MultipleChoiceApp/Teacher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleChoiceApp
+{
+    class Teacher : Person
+    {
+        private string subject;
+
+        
+
+        public Teacher(string name, string subject) : base(name)
+        {
+            this.subject = subject;
+        }
+
+        public override string ReturnInfo()
+        {
+            string info;
+            info = String.Format("Name : {0}\nStudent Number : {1}", name, subject);
+            return info;
+        }
+    }
+}
