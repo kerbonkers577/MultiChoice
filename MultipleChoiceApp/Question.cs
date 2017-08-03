5131a94718e0dab63e2dada76279bb051c035f15
Tristan Constable <tristan.m.constable@gmail.com>
2017-08-03 10:41:24 +0200

    Question Methods added

--- MultipleChoiceApp/Question.cs
+++ MultipleChoiceApp/Question.cs
@@ -9,11 +9,78 @@ namespace MultipleChoiceApp
     class Question
     {
         private string questionText;
-        private string Answer1;
-        private string Answer2;
-        private string Answer3;
-        private string Answer4;
+        private string answer1;
+        private string answer2;
+        private string answer3;
+        private string answer4;
+        private int actualAnswer;
 
+        public Question(string questionText, string answer1, string answer2, string answer3, string answer4, int actualAnswer)
+        {
+            //Create Singular Question for test
+            this.questionText = questionText;
+            this.answer1 = answer1;
+            this.answer2 = answer2;
+            this.answer3 = answer3;
+            this.answer4 = answer4;
+            this.actualAnswer = actualAnswer;
+        }
 
+        //Accessors
+        public string GetQuestionText()
+        {
+            return questionText;
+        }
+
+        public string GetAnswer1Text()
+        {
+            return answer1;
+        }
+
+        public string GetAnswer2Text()
+        {
+            return answer2;
+        }
+
+        public string GetAnswer3Text()
+        {
+            return answer3;
+        }
+
+        public string GetAnswer4Text()
+        {
+            return answer4;
+        }
+
+        public int GetActualAnswer()
+        {
+            return actualAnswer;
+        }
+
+        //Mutators
+        public void SetQuestionText(string questionText)
+        {
+            this.questionText = questionText;
+        }
+
+        public void SetAnswer1Text(string answer1)
+        {
+            this.answer1 = answer1;
+        }
+
+        public void SetAnswer2Text(string answer2)
+        {
+            this.answer2 = answer2;
+        }
+
+        public void SetAnswer3Text(string answer3)
+        {
+            this.answer3 = answer3;
+        }
+
+        public void SetAnswer4Text(string answer4)
+        {
+            this.answer4 = answer4;
+        }
     }
 }
