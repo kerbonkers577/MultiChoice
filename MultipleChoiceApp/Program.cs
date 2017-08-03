2577490fcd141b9968242433524a3d60f24d5b9b
Tristan Constable <tristan.m.constable@gmail.com>
2017-08-03 11:10:42 +0200

    Basic Interface

--- MultipleChoiceApp/Program.cs
+++ MultipleChoiceApp/Program.cs
@@ -10,7 +10,28 @@ namespace MultipleChoiceApp
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Welcome to the multiple choice application\nAre you a:\n(1) Student\n(2) Teacher");
+            DisplayUserFunctionality();
+
+            int response = Convert.ToInt16(Console.ReadKey());
+
+            if(response == 1)
+            {
+
+            }
+            else if(response == 2)
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void DisplayUserFunctionality()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Enter an integer value (e.g 1, 2, 3, 4 etc.) to give an answer");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
