67ca970d49a342625550cc6b4490d2fcc2b0cf55
Tristan Constable <tristan.m.constable@gmail.com>
2017-10-01 11:10:47 +0200

    Access Class Created

---
+++ MultipleChoiceLibrary/DataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MultipleChoiceLibrary
+{
+    class DataAccess
+    {
+        //Student table access
+        //StudentAnswer table access
+        //Question table access
+        //Teacher table access
+    }
+}
